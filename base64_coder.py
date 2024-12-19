a6f086192c3b08b69905179fbc1c3acb840e4755
MrRighter <kirill.a.shikunov@gmail.com>
2024-12-19 23:10:22 +0300

    updates

--- base64_coder.py
+++
@@ -1,8 +0,0 @@
-import base64
-
-with open("ИМЯ/ПУТЬ", "rb") as image_file:
-    encoded_string = base64.b64encode(image_file.read())
-    # decoded_string = base64.b64decode(encoded_string)
-
-print(encoded_string)
-# print(decoded_string)
